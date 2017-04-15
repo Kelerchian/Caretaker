3e23d540b936559f716a0072389f04992eeaa1e7
Alan Darmasaputra <kelerchian@gmail.com>
2017-04-15 19:49:16 +0700

    update to dist and example

--- dist/caretaker.js
+++ dist/caretaker.js
@@ -55200,7 +55200,6 @@ class CaretakerInput extends React.Component{
 			this.state.value = ""
 		}
 		this.loadValue(props)
-			console.log(props)
 	}
 	componentDidMount(){
 		this.updateParent()
@@ -55242,9 +55241,9 @@ class CaretakerInput extends React.Component{
 			case "week"											: return true; //not implemented
 			//need options
 			case "select"										: return false;
-			case "select-multiple"					: return false;
+			case "select-multiple"					: return true;
 			case "checkbox"									: return false;
-			case "textarea"									: return false;
+			case "textarea"									: 
 			case "textarea-text"						: return false;
 			case "textarea-html"						: return false;
 			case "radio"										: return false;
@@ -55275,7 +55274,7 @@ class CaretakerInput extends React.Component{
 			case "date"											: return React.createElement(CaretakerFormInputDate, this.getSpecialProps()); break;
 			case "week"											: break;
 			//need options
-			case "select"										: break;
+			case "select"										: return React.createElement(CaretakerFormInputSelect, this.getSpecialProps()); break;
 			case "select-multiple"					: break;
 			case "checkbox"									: return React.createElement(CaretakerFormInputCheckbox, this.getSpecialProps()); break;
 			case "textarea"									:
@@ -55437,7 +55436,7 @@ class CaretakerFormObject extends React.Component{
 					objects.push( React.createElement(CaretakerFormObject, childProps) )
 				}
 			}
-			return React.createElement('div',{className:"CaretakerFormObject", "key":"object"}, objects)
+			return objects
 		}else{
 			var props = this.getInputProps()
 			props.key = "object"
@@ -55526,8 +55525,9 @@ class CaretakerFormObjectCollection extends React.Component{
 		}
 	}
 	appearanceGetControl(){
-		return React.createElement('div',{className:"CaretakerFormObjectCollectionControl", key:"control"}, (
-			React.createElement('button',{className:"CaretakerFormObjectCollectionAdd", "type":"button", onClick:this.onAddChild.bind(this)}, "Add New")
+		var name = this.props.name || ""
+		return React.createElement('div',{className:"CaretakerFormObjectCollectionControl "+name, key:"control"}, (
+			React.createElement('button',{className:"CaretakerButton CaretakerAddObjectButton", "type":"button", onClick:this.onAddChild.bind(this)}, "Add New")
 		))
 	}
 	appearanceGetChildren(){
@@ -55543,7 +55543,7 @@ class CaretakerFormObjectCollection extends React.Component{
 				props.value = this.state.value[i]
 			}
 			children.push( React.createElement('div', { className: "CaretakerFormObjectContainer", key: i}, [
-				React.createElement('button', { onClick:this.onRemoveChild.bind(this,i), type:"button" , key:i+"-delete-button" }, "delete"),
+				React.createElement('button', { className:"CaretakerButton CaretakerRemoveObjectButton", onClick:this.onRemoveChild.bind(this,i), type:"button" , key:i+"-delete-button" }, "Delete"),
 				React.createElement(CaretakerFormObject, props)
 			]) )
 		}
@@ -55795,6 +55795,87 @@ class CaretakerFormInputRadio extends React.Component{
 		return React.createElement('div', {className: "CaretakerFormInputRadioCollection"}, (
 			this.getCheckboxes()
 		))
+	}
+}
+
+/** Command example
+{
+	type:"select",
+	name:"gender"
+	values:[
+		{value:"male", text:"Male"},
+		{value:"female", text:"Female"}
+	],
+	defaultValue: "male"
+}
+*/
+class CaretakerFormInputSelect extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {}
+		this.loadValue(props)
+	}
+	componentDidMount(){
+		this.updateParent()
+	}
+	componentWillReceiveProps(props){
+		this.loadValue(props)
+		this.setState(this.state)
+	}
+	loadValue(props){
+		this.state.value = ""
+		if(props.value){
+			this.state.value = props.value
+		}else if(props.defaultValue){
+			this.state.value = props.defaultValue
+		}
+	}
+	updateParent(){
+		if(this.props.onChange){
+			this.props.onChange(this.state.value)
+		}
+		this.setState(this.state)
+	}
+	onChange(event){
+		this.state.value = event.target.value
+		this.updateParent()
+	}
+	getNegativePropKeys(){
+		return ["value","values","multiple"]
+	}
+	getProps(){
+		var props = Object.assign({}, this.props)
+		this.getNegativePropKeys().forEach(function(key){
+			props[key] = null
+			delete props[key]
+		})
+		props.value = this.state.value
+		props.onChange = this.onChange.bind(this)
+		return props
+	}
+	appearanceGetSelect(){
+		var options = this.props.values || {}
+		if(typeof options != "object"){
+			options = {}
+		}
+
+		var optionElements = []
+		for(var i in options){
+			var option = options[i]
+			var value = option.value || i
+			var key = option.value || i
+			var text = option.text || i
+			var prop = {value:value, key:key}
+			optionElements.push(React.createElement('option',prop, text))
+		}
+
+		var props = this.getProps()
+		console.log(props)
+		var select = React.createElement('select', props, optionElements)
+		return select
+	}
+	render(){
+		return this.appearanceGetSelect()
 	}
 }
 
