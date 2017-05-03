795f3508de8fe2ff498da17e70b9e6ce8bcb47b0
Alan Darmasaputra <kelerchian@gmail.com>
2017-05-03 17:00:49 +0700

    updated textarea

--- factory/textarea_dependency/textarea_raw.js
+++ factory/textarea_dependency/textarea_raw.js
@@ -3,43 +3,12 @@ import undoPlugin from 'draft-js-undo-plugin';
 import linkifyPlugin from 'draft-js-linkify-plugin';
 import createInlineToolbarPlugin, {Separator} from 'draft-js-inline-toolbar-plugin';
 import linkifyIt from 'linkify-it'
-import { EditorState, ContentState, convertToRaw, convertFromHTML } from 'draft-js';
+import { EditorState, RichUtils, ContentState, convertToRaw, convertFromHTML } from 'draft-js';
 import { convertToHTML } from 'draft-convert';
-import { ItalicButton,
-  BoldButton,
-  UnderlineButton,
-  CodeButton,
-  HeadlineOneButton,
-  HeadlineTwoButton,
-  HeadlineThreeButton,
-  UnorderedListButton,
-  OrderedListButton,
-  BlockquoteButton,
-	AlignBlockCenterButton,
-	AlignBlockDefaultButton,
-	AlignBlockLeftButton,
-	AlignBlockRightButton
-} from 'draft-js-buttons';
-var inlineToolbarPluginParams = {
-  structure: [
-    BoldButton,
-    ItalicButton,
-    UnderlineButton,
-    CodeButton,
-    Separator,
-    HeadlineOneButton,
-    HeadlineTwoButton,
-    HeadlineThreeButton,
-    UnorderedListButton,
-    OrderedListButton,
-    BlockquoteButton
-  ]
-}
+
 var pluginsHTML = {
 	createUndoPlugin: undoPlugin,
 	createLinkifyPlugin: linkifyPlugin,
-	createInlineToolbarPlugin: createInlineToolbarPlugin,
-	inlineToolbarPluginParams: inlineToolbarPluginParams
 }
 
 
@@ -51,6 +20,7 @@ var Textarea = {
 	convertToRaw: convertToRaw,
 	convertFromHTML: convertFromHTML,
 	convertToHTML: convertToHTML,
-	linkifyIt: linkifyIt
+	linkifyIt: linkifyIt,
+	RichUtils: RichUtils
 }
 window.CaretakerTextareaDependency = Textarea
